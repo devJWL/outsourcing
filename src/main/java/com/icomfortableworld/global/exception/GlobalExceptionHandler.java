6a2c4678f1a8425b71f8f6d9801f954b7cc9e685
devJWL <jwlee4450@gmail.com>
2024-03-05 20:23:36 +0900

    🐛 fix: 매소드 반환타입 변경
     ResponseEntity<CommonResponseDto<Void>> -> ErrorResponseDto<String>

--- src/main/java/com/icomfortableworld/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/icomfortableworld/global/exception/GlobalExceptionHandler.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.icomfortableworld.common.dto.CommonResponseDto;
 import com.icomfortableworld.domain.feed.exception.CustomFeedException;
 import com.icomfortableworld.domain.member.exception.CustomMemberException;
 import com.icomfortableworld.global.exception.dto.ErrorResponseDto;
@@ -20,7 +18,7 @@ import com.icomfortableworld.jwt.exception.CustomJwtException;
 public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ErrorResponseDto<List<String>> methodArgumentNotValidException(
-		MethodArgumentNotValidException e) {
+			MethodArgumentNotValidException e) {
 		List<String> errorList = new ArrayList<>();
 		for (FieldError error : e.getBindingResult().getFieldErrors()) {
 			errorList.add(error.getDefaultMessage());
@@ -29,17 +27,17 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler(CustomJwtException.class)
-	public ResponseEntity<CommonResponseDto<Void>> customJwtExceptionHandler(CustomJwtException e) {
-		return CommonResponseDto.of(HttpStatus.UNAUTHORIZED, e.getMessage(), null);
+	public ErrorResponseDto<String> customJwtExceptionHandler(CustomJwtException e) {
+		return ErrorResponseDto.of(HttpStatus.UNAUTHORIZED, e.getMessage());
 	}
 
 	@ExceptionHandler(CustomMemberException.class)
-	public ResponseEntity<CommonResponseDto<Void>> customMemberExceptionHandler(CustomMemberException e) {
-		return CommonResponseDto.of(HttpStatus.BAD_REQUEST, e.getMessage(), null);
+	public ErrorResponseDto<String> customMemberExceptionHandler(CustomMemberException e) {
+		return ErrorResponseDto.of(HttpStatus.BAD_REQUEST, e.getMessage());
 	}
 
 	@ExceptionHandler(CustomFeedException.class)
-	public ResponseEntity<CommonResponseDto<Void>> customFeedExceptionHandler(CustomFeedException e) {
-		return CommonResponseDto.of(HttpStatus.BAD_REQUEST, e.getMessage(), null);
+	public ErrorResponseDto<String> customFeedExceptionHandler(CustomFeedException e) {
+		return ErrorResponseDto.of(HttpStatus.BAD_REQUEST, e.getMessage());
 	}
 }

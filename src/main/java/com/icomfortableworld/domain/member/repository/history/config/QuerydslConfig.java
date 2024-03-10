22693a551b177b81913146902194725eb00d47d5
devJWL <jwlee4450@gmail.com>
2024-03-10 12:51:56 +0900

    🐛 feat(querydsl): @PersistenceContext을 이용하여 thread safe 되게 수정

--- src/main/java/com/icomfortableworld/domain/member/repository/history/config/QuerydslConfig.java
+++ src/main/java/com/icomfortableworld/domain/member/repository/history/config/QuerydslConfig.java
@@ -2,15 +2,15 @@ package com.icomfortableworld.domain.member.repository.history.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class QuerydslConfig {
 
-    private final EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
